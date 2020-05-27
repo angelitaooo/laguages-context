4891ff75e6cf4e2f628db46b564da3229cfc5546
Angela Ordonez <angelitaooo@me.com>
2020-05-26 19:48:17 -0500

    change languages using cotext

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import UserCreate from './UserCreate';
+import LanguageContext from '../context/LanguageContext';
+import ColorContext from '../context/ColorContext';
+
+class App extends React.Component {
+  state = {
+    language: 'english'
+  }
+
+  onLanguageChange = language => {
+    this.setState({language})
+  }
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i className="flag us" onClick={() => this.onLanguageChange('english')}></i>
+          <i className="flag nl" onClick={() => this.onLanguageChange('dutch')}></i>
+        </div>
+        <LanguageContext.Provider value={this.state.language}>
+          <ColorContext.Provider value="primary"> {/*here I can change the color of the button */}
+            <UserCreate></UserCreate>
+          </ColorContext.Provider>
+        </LanguageContext.Provider>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import LanguageContext from '../context/LanguageContext';
+import ColorContext from '../context/ColorContext';
+
+class Button extends React.Component {
+  // example using the Consumer
+  renderButton = (color) => {
+    return (
+      <button className={`ui button ${color}`}>
+        <LanguageContext.Consumer>
+          {/* always going to be a funtion */}
+          {(value) => (value === 'english' ? 'Submit' : 'Voorleggen')}
+        </LanguageContext.Consumer>
+      </button>
+    );
+  };
+  render() {
+    return (
+      <ColorContext.Consumer>
+        {(color) => this.renderButton(color)}
+      </ColorContext.Consumer>
+    );
+  }
+}
+
+export default Button;
